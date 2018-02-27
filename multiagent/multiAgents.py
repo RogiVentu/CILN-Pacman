6630295c596bedad754f4418dd0fbe12aad8c1ca
Roger Ventura Castelló <roger.ventura01@estudiant.upf.edu>
2018-02-27 17:43:13 +0100

    q2 done bro

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -152,6 +152,38 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Returns the total number of agents in the game
         """
         "*** YOUR CODE HERE ***"
+        #there we define 2 methods that represent min_value:ghosts players and max_value: pacman player. 
+        def min_value(gameState, depth, ghostIndex): #min player method, the ghosts.
+            if gameState.isWin() or gameState.isLose():
+                return self.evaluationFunction(gameState);
+
+            min_aux = float('Inf')
+            numGhosts = gameState.getNumAgents()-1 #because we just want the ghosts
+            #as the labs says: the minimax tree will have multiple min layers (one for each ghost) for every max layer.
+            #so we need to recursively all the ghosts and the last one just call max_value method
+            for action in gameState.getLegalActions(ghostIndex):
+                if ghostIndex is not numGhosts:
+                    min_aux = min(min_aux , min_value(gameState.generateSuccessor(ghostIndex, action), depth, ghostIndex+1))
+                else:
+                    min_aux = min(min_aux , max_value(gameState.generateSuccessor(ghostIndex, action), depth+1))
+            return min_aux
+
+        def max_value(gameState, depth): #max player method, pacman.
+            if gameState.isWin() or gameState.isLose() or depth == self.depth:
+                return self.evaluationFunction(gameState)
+
+            max_aux = float('-Inf')
+            for action in gameState.getLegalActions(0): #0 because its Pacman
+                #compare actual max with the min value of the successors from each action, and then this will be recursivea and will get the minimun value
+                max_aux = max(max_aux , min_value(gameState.generateSuccessor(0, action), depth, 1)) # 1 because ghostIndex its >=1
+            return max_aux
+
+        #here we get the action with max value using the 2 methods before
+        min_value_action = [] #we store here a touples of (value,action)
+        for action in gameState.getLegalActions(0):
+            min_value_action.append((min_value(gameState.generateSuccessor(0, action), 0, 1), action)) #with depth = 0
+        return max(min_value_action,key=lambda item:item[0])[1] #return the action with max value
+
         util.raiseNotDefined()
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
@@ -164,6 +196,35 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
           Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+
+        def min_value(gameState, depth, ghostIndex): #min player method, the ghosts.
+            if gameState.isWin() or gameState.isLose():
+                return self.evaluationFunction(gameState);
+
+            min_aux = float('Inf')
+            numGhosts = gameState.getNumAgents()-1 
+            for action in gameState.getLegalActions(ghostIndex):
+                if ghostIndex is not numGhosts:
+                    min_aux = min(min_aux , min_value(gameState.generateSuccessor(ghostIndex, action), depth, ghostIndex+1))
+                else:
+                    min_aux = min(min_aux , max_value(gameState.generateSuccessor(ghostIndex, action), depth+1))
+            return min_aux
+
+        def max_value(gameState, depth):
+            if gameState.isWin() or gameState.isLose() or depth == self.depth:
+                return self.evaluationFunction(gameState)
+
+            max_aux = float('-Inf')
+            for action in gameState.getLegalActions(0):
+                max_aux = max(max_aux , min_value(gameState.generateSuccessor(0, action), depth, 1))
+            return max_aux
+
+        min_value_action = [] 
+        for action in gameState.getLegalActions(0):
+            min_value_action.append((min_value(gameState.generateSuccessor(0, action), 0, 1), action))
+        return max(min_value_action,key=lambda item:item[0])[1]
+
+        util.raiseNotDefined()
         util.raiseNotDefined()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
